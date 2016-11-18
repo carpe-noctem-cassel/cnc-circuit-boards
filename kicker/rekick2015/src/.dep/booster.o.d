6ea857a56be30cd3eb1fe7aa1fa4f67233536327
Carpe Noctem <cn@das-lab.net>
2016-11-18 15:06:39 +0100

    moved from msl_electronic_boards to new repository

---
+++ kicker/rekick2015/src/.dep/booster.o.d
@@ -0,0 +1,59 @@
+booster.o: booster.c booster.h /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/4.9.2/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /usr/lib/avr/include/avr/iom16m1.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
+ /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
+ messages.h can_lib.h can_drv.h config.h \
+ /usr/lib/avr/include/avr/interrupt.h defaults.h global.h \
+ /usr/lib/avr/include/util/delay.h \
+ /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/math.h \
+ /usr/lib/avr/include/string.h /usr/lib/gcc/avr/4.9.2/include/stddef.h
+
+booster.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/4.9.2/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/avr/include/avr/iom16m1.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+messages.h:
+
+can_lib.h:
+
+can_drv.h:
+
+config.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+defaults.h:
+
+global.h:
+
+/usr/lib/avr/include/util/delay.h:
+
+/usr/lib/avr/include/util/delay_basic.h:
+
+/usr/lib/avr/include/math.h:
+
+/usr/lib/avr/include/string.h:
+
+/usr/lib/gcc/avr/4.9.2/include/stddef.h:
